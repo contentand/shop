4d4874dcdb025cfb4dc1d8b03d0fccc8201d96ef
Daniil Yurov <daniil.yurov@travisperkins.co.uk>
2018-01-10 18:34:44 +0200

    add simple properties to ProductComponent

---
+++ src/app/category.ts
@@ -0,0 +1,4 @@
+export enum Category {
+    Category1 = 'Category 1',
+    Category2 = 'Category 2'
+}
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from '../category';
 
 @Component({
   selector: 'app-product',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
+  name: string;
+  description: string;
+  price: number;
+  category: Category;
+  isAvailable: boolean;
+
   constructor() { }
 
   ngOnInit() {
+    this.name = 'Product A';
+    this.description = 'Product A is an awesome product to buy!';
+    this.price = 99.99;
+    this.category = Category.Category1;
+    this.isAvailable = true;
   }
 
 }
