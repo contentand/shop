0c819c2ed6ad29c062f4b4cf49aaef919202d161
Daniil Yurov <daniil.yurov@travisperkins.co.uk>
2018-01-30 19:55:49 +0200

    T4: add async and number to hot offer count down

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 * ProductComponent and CartComponent have currency pipe in template
 * CartComponent has date pipe
 * ProductComponent has uppercase pipe
+* CartComponent has async and number pipes to show hot offer count down.
--- src/app/cart/component/cart/cart.component.ts
+++ src/app/cart/component/cart/cart.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../../../product/index';
 import { Cart, CartUpdate, CartItem } from '../../model';
 import { CartService } from '../../service';
+import { Observable } from 'rxjs/Observable';
+import { Subscriber } from 'rxjs/Subscriber';
 
 @Component({
   selector: 'app-cart',
@@ -11,6 +13,11 @@ import { CartService } from '../../service';
 export class CartComponent implements OnInit {
 
   cart: Cart;
+
+  time = new Observable<string>((observer: Subscriber<string>) => {
+    let counter = 3000;
+    setInterval(() => observer.next((counter--).toString()), 1000);
+  });
 
   constructor(private cartService: CartService) { }
 
