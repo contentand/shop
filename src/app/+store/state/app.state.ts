cbc64fb85e6f5268c9607dc6614082a927c23eba
Daniil Yurov <daniil.yurov@travisperkins.co.uk>
2018-03-01 19:05:27 +0200

    T7: define action reducer map

--- src/app/+store/state/app.state.ts
+++ src/app/+store/state/app.state.ts
@@ -1,6 +1,11 @@
 import { BooksState } from './books.state';
+import { ActionReducerMap } from '@ngrx/store';
+import { booksReducer } from '../reducer/books.reducer';
 
 export interface ApplicationState {
     books: BooksState;
 }
 
+export const reducers: ActionReducerMap<ApplicationState> = {
+    books: booksReducer
+};
